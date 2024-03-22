f7777a6f854c5eba48e7a6c5e46d60c102fa1597
Marc-Philipp Esser <m-esser@mail.de>
2024-03-22 17:26:28 +0100

    add stage models

---
+++ models/stage/looker_ecommerce/vw_stg_looker_ecommerce__events.sql
@@ -0,0 +1,19 @@
+{%- set yaml_metadata -%}
+source_model: 'vw_rstg_looker_ecommerce__events'
+derived_columns:
+    DV_RECSOURCE: TO_CHAR('Looker Ecommer')
+    DV_LOADTS: CURRENT_TIMESTAMP(0)
+    DV_USERID: CURRENT_ACCOUNT()
+hashed_columns:
+    DV_HASHKEY_EVENT: 'ID'
+    DV_HASHKEY_USER: 'USER_ID'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     null_columns=none,
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
