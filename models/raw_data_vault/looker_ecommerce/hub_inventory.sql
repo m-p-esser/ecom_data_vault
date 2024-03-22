76fd1b11f06f9590da35709a7312538f072b0e65
Marc-Philipp Esser <m-esser@mail.de>
2024-03-22 20:59:26 +0100

    replace double quotes with single quotes

--- models/raw_data_vault/looker_ecommerce/hub_inventory.sql
+++ models/raw_data_vault/looker_ecommerce/hub_inventory.sql
@@ -1,10 +1,10 @@
 {{ config(materialized='incremental')    }}
 
-{%- set source_model = "vw_stg_looker_ecommerce__inventory_items"   -%}
-{%- set src_pk = "DV_HASHKEY_INVENTORY_ITEM"          -%}
-{%- set src_nk = "ID"          -%}
-{%- set src_ldts = "DV_LOADTS"      -%}
-{%- set src_source = "DV_RECSOURCE"    -%}
+{%- set source_model = 'vw_stg_looker_ecommerce__inventory_items'   -%}
+{%- set src_pk = 'DV_HASHKEY_INVENTORY_ITEM'          -%}
+{%- set src_nk = 'ID'          -%}
+{%- set src_ldts = 'DV_LOADTS'      -%}
+{%- set src_source = 'DV_RECSOURCE'    -%}
 
 {{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
                    src_source=src_source, source_model=source_model) }}
