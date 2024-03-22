dd00c17f445f495835605d78b9f16c9862661c6a
Marc-Philipp Esser <m-esser@mail.de>
2024-03-22 19:54:21 +0100

    add remaining stage models which hash business keys and add metadata

---
+++ models/stage/looker_ecommerce/vw_stg_looker_ecommerce__distribution_centers.sql
@@ -0,0 +1,18 @@
+{%- set yaml_metadata -%}
+source_model: 'vw_rstg_looker_ecommerce__distribution_centers'
+derived_columns:
+    DV_RECSOURCE: TO_CHAR('Looker Ecommerce')
+    DV_LOADTS: CURRENT_TIMESTAMP(0)
+    DV_USERID: CURRENT_ACCOUNT()
+hashed_columns:
+    DV_HASHKEY_DISTRIBUTION_CENTER: 'ID'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     null_columns=none,
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
---
+++ models/stage/looker_ecommerce/vw_stg_looker_ecommerce__order_items.sql
@@ -0,0 +1,22 @@
+{%- set yaml_metadata -%}
+source_model: 'vw_rstg_looker_ecommerce__order_items'
+derived_columns:
+    DV_RECSOURCE: TO_CHAR('Looker Ecommerce')
+    DV_LOADTS: CURRENT_TIMESTAMP(0)
+    DV_USERID: CURRENT_ACCOUNT()
+hashed_columns:
+    DV_HASHKEY_ORDER_ITEM: 'ID'
+    DV_HASHKEY_ORDER: 'ORDER_ID'
+    DV_HASHKEY_USER: 'USER_ID'
+    DV_HASHKEY_PRODUCT: 'PRODUCT_ID'
+    DV_HASHKEY_INVENTORY: 'INVENTORY_ITEM_ID'
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{ automate_dv.stage(include_source_columns=true,
+                     source_model=metadata_dict['source_model'],
+                     derived_columns=metadata_dict['derived_columns'],
+                     null_columns=none,
+                     hashed_columns=metadata_dict['hashed_columns'],
+                     ranked_columns=none) }}
